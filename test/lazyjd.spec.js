2626d6e62fff2bac3dc3445b6f6322b1b543fd8d
chris <chris@bumblehead.com>
2014-03-02 15:49:35 -0800

    removed readme.html which npm has difficulty ignoring

--- test/lazyjd.spec.js
+++ test/lazyjd.spec.js
@@ -1,5 +1,5 @@
 // Filename: lazycon.spec.js  
-// Timestamp: 2014.02.02-14:07:58 (last modified)  
+// Timestamp: 2014.02.03-22:54:50 (last modified)  
 // Author(s): Bumblehead (www.bumblehead.com)  
 
 var lazycon = require('../lazycon');
@@ -44,6 +44,43 @@ describe("lazycon", function () {
       constructedobj.method1() === 'thisproperty1',
       constructedobj.method2() === 'thisproperty2'
     ).toBe( true );
+
+  });
+
+
+  it("should define constructor data sequentially", function (done) {  
+
+    var constructor1 = lazycon().defProtoFn({
+
+    }).defConstructFn(function (me, arg1) {
+      me.property1 = '1';
+      return me;
+    });
+
+    var constructor2 = lazycon(
+      constructor1
+    ).defProtoFn({
+
+    }).defConstructFn(function (me, arg1) {
+      me.property2 = '2';
+      return me;
+    });
+
+    var constructor3 = lazycon(
+      constructor2
+    ).defProtoFn({
+
+    }).defConstructFn(function (me, arg1) {
+      me.property3 = '3';
+//      expect(me.property2).toBe( '2' );
+//      expect(me.property1).toBe( '1' );
+      done();
+
+      return me;
+    });
+
+    constructor3.getNew();
+
 
   });
 });
